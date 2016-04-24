6d6f6d211b8baa1471e806f2749dba8a4c096cbd
ElenaZakharova47 <ElenaZakharova47@gmail.com>
2016-04-24 18:13:36 +0300

    third commit
    add album orientational

--- app/src/main/java/com/example/android/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/example/android/scientificcalculator/MainActivity.java
@@ -2,6 +2,27 @@ package com.example.android.scientificcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.app.Activity;
+//add the import statements here to import the Button, TextView and View classes
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import android.app.Activity;
+import android.media.AudioManager;
+import android.util.Log;
+
+import android.widget.GridLayout;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
